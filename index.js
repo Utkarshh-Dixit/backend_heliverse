46d9bf36646a0b3ada1230b2ed1c1c31d8ec0c53
Utkarsh Dixit <utkarshdixit.2k21@gmail.com>
2024-04-01 16:28:29 +0530

    Frontend

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const fs = require('fs');
 require('dotenv').config();
+const userData = require('./userData.json');
+
+const User = require('./models/User');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -19,19 +22,33 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Read user data from JSON file
-const userData = JSON.parse(fs.readFileSync('userData.json', 'utf-8'));
+// const userData = JSON.parse(fs.readFileSync('userData.json', 'utf-8'));
 
 // Routes
-app.get('/api/users', (req, res) => {
-  // Pagination logic
-  const page = parseInt(req.query.page) || 1;
-  const pageSize = 20;
-  const startIndex = (page - 1) * pageSize;
-  const endIndex = page * pageSize;
+app.get('/api/users', async (req, res) => {
+  try {
+    // Pagination logic
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = 20;
+    const startIndex = (page - 1) * pageSize;
+    const endIndex = page * pageSize;
 
-  const paginatedUsers = userData.slice(startIndex, endIndex);
-  
-  res.json(paginatedUsers);
+    let filteredUsers = userData;
+
+    // Search by name
+    if (req.query.search) {
+      const searchRegex = new RegExp(req.query.search, 'i'); // Case-insensitive search
+      filteredUsers = userData.filter(user => searchRegex.test(user.first_name) || searchRegex.test(user.last_name));
+    }
+
+    const paginatedUsers = filteredUsers.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(filteredUsers.length / pageSize);
+
+    res.json({ users: paginatedUsers, totalPages });
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 app.get('/api/users/:id', (req, res) => {
